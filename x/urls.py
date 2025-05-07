4e38a744a0453977cee35f790366c688ffd58d9b
yamuna <yamuna.pdy@gmail.com>
2025-05-07 15:21:57 +0300

    my first commit

--- x/urls.py
+++ x/urls.py
@@ -3,12 +3,13 @@ from .import views
 from x.views import xListView, xAdd, xDelete, xEditView, xLikeAdd, xLikeSubtract
 
 urlpatterns = [
-   path('first/', xListView),
-    path('xAdd/', xAdd),
-    path('xDelete/<int:x_id>/', xDelete),
-    path('xEditView/<int:x_id>/', xEditView),
-    path('xLikeAdd/<int:x_id>/', xLikeAdd),
-    path('xLikeSubtract/<int:x_id>/', xLikeSubtract),
-]
+   path('first/', xListView, name='post1'),
+    path('xAdd/', xAdd, name='x_add'),
+    path('xDelete/<int:x_id>/', xDelete, name='x_delete'),
+    path('xEditView/<int:x_id>/', xEditView, name='x_edit'),
+    path('xLikeAdd/<int:x_id>/', xLikeAdd, name='x_like_add'),
+    # path('api/xLikeAdd/<int:x_id>/', xLikeAdd, name='like-add')
+    path('xLikeSubtract/<int:x_id>/', xLikeSubtract, name='x_like_subtract'),
+] 
 
 
--- x/views.py
+++ x/views.py
@@ -10,6 +10,11 @@ from pprint import pprint
 from datetime import datetime
 from .forms import XForm
 import cloudinary
+from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
+
+
 
 def xListView(request):
   # Get all xs, limit = 20
@@ -29,59 +34,47 @@ def xEditView(request, x_id):
     if form.is_valid():
       # Save and redirect to home
       form.save()
-      return HttpResponseRedirect('/')
+      # return HttpResponseRedirect('second/first/')
+      return redirect('post1')
     else:
       print(form.errors)
 
   else:
     # Show editting screen
     form = XForm
-    return render(request, 'x_edit.html',
+  return render(request, 'x/x_edit.html', 
     {'x': x, 'form': form})
 
-def xAdd(request):
-  # If the method is POST
-  if request.method == 'POST':
-    form = XForm(request.POST, request.FILES)
-    if form.is_valid():
-      # Save and redirect to home
-      form.save()
-    else:
-      print(form.errors)
 
-  return HttpResponseRedirect('/')
+def xAdd(request):
+    if request.method == 'POST':
+        form = XForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+    return redirect('post1')  # Use name from urls.py
 
 def xDelete(request, x_id):
-  # Get x
-  x_to_delete = X.objects.get(id=x_id)
-  
-  # Delete
-  x_to_delete.delete()
+    x_to_delete = get_object_or_404(X, id=x_id)
+    x_to_delete.delete()
+    
+    return redirect('post1') 
 
-  return HttpResponseRedirect('/')
 
+
+@require_POST
 def xLikeAdd(request, x_id):
-  # Get requested x
-  x = X.objects.get(id = x_id)
+    x = get_object_or_404(X, id=x_id)
+    x.like_count += 1
+    x.save()
+    return JsonResponse({'like_count': x.like_count})
 
-  # Add count
-  new_like_count = x.like_count + 1
-  x.like_count = new_like_count
 
-  # Save
-  x.save()
-
-  return JsonResponse({'result': 'successful'})
-
+@require_POST
 def xLikeSubtract(request, x_id):
-  # Get requested x
-  x =X.objects.get(id = x_id)
-
-  # Subtract count
-  new_like_count = x.like_count - 1
-  x.like_count = new_like_count
-
-  # Save
-  x.save()
-
-  return JsonResponse({'result': 'successful'})
+    x = get_object_or_404(X, id=x_id)
+    if x.like_count > 0:
+        x.like_count -= 1
+        x.save()
+    return JsonResponse({'like_count': x.like_count})
