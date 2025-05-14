93311aeedfae05fa9fb8cc36e6a7faa098431647
yamuna <yamuna.pdy@gmail.com>
2025-05-14 12:10:50 +0300

    my first commit

--- Xclone/settings.py
+++ Xclone/settings.py
@@ -155,6 +155,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # if you have a local sta
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will dump files
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
